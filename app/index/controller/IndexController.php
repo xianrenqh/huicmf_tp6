2aba0c7252b17d1a082a92429bdff48956c32adb
xianrenqh <xianrenqh@163.com>
2021-02-20 12:04:40 +0800

    test
    test - huicmf-6

---
+++ app/index/controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 小灰灰
+ * Date: 2021-02-19
+ * Time: 18:04:37
+ * Info:
+ */
+
+namespace app\index\controller;
+
+class IndexController
+{
+
+    public function index()
+    {
+        return 12;
+    }
+}
